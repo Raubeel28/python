ecc02b1c4c9a1ee5f4cb881a5a9aca940216233a
Raubeel28 <siniorchair@gmail.com>
2025-05-01 07:20:00 -0700

    https://github.com/Raubeel28/python.git

---
+++ main.py
@@ -0,0 +1,21 @@
+principal=0
+time = 0
+rate= 0
+while principal <=0: 
+  principal= float(input ("Enter the principal amount"))
+if principal <= 0:
+  print ("Principal cannot be a negative")
+
+while rate <=0: 
+ rate= float(input ("Enter the rate amount"))
+if principal <= 0:
+  print ("rate cannot be a negative")
+   
+while time<=0: 
+  time= int(input ("Enter the time amount"))
+if principal <= 0:
+  print ("time cannot be a negative or zero")
+   
+print (principal)
+print (time)
+print (rate)
