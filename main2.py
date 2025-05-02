00e3594e11c00f9c4770b0958eb2a770eeb2b945
Raubeel28 <siniorchair@gmail.com>
2025-05-02 12:52:40 -0700

    lesson

---
+++ main2.py
@@ -0,0 +1,19 @@
+#fruits = ["apple","pineapple","beans","orange"]
+#fruits.append("kontombire")
+#fruits.remove("apple")
+#fruits.insert(3,"apple")
+#fruits.sort()
+#fruits.reverse()
+#print (fruits)
+#print(fruits.count("apple"))
+#print(fruits.index("apple"))
+
+fruits = {"banana","orange","coconut"}
+fruits.add("pineapple")
+fruits.pop()
+print(fruits)
+
+for fruit in fruits:
+    print(fruit,end=" ")
+print()
+
