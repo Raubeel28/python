a11006d2323a813bf31d0e574b3ff1dc7040ca2b
Raubeel28 <siniorchair@gmail.com>
2025-05-22 12:42:02 -0700

    gUi

---
+++ GUI.py
@@ -0,0 +1,49 @@
+import sys
+from  PyQt5.QtWidgets import QMainWindow ,QApplication, QLabel
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(700,300,500,500)
+    
+        
+        label = QLabel("Hello",self)
+        label.setFont(QFont("Arial",20))
+        label.setGeometry(0,0,500,100)
+        label.setStyleSheet("color:blue;"
+                            "background-color:grey;"
+                            "font-weight:bold;"
+                            "font-style:italic;"
+                            "text-decoration: underline")
+        #label.setAlignment(Qt.AlignBottom)  
+       # label.setAlignment(Qt.AlignTop)
+        #label.setAlignment(Qt.AlignVCenter)
+        #label.setAlignment(Qt.AlignRight)
+        #label.setAlignment(Qt.AlignHCenter)
+        #label.setAlignment(Qt.AlignHCenter |Qt.AlignBottom)
+        label.setAlignment(Qt.AlignCenter) #THis aligns the text vertically and horizotally to the middle
+        
+
+       
+
+
+
+def main():
+    app=QApplication(sys.argv)
+    window=MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
+
+
